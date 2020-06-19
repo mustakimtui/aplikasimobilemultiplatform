4d48c232944d45a3456d962aa2a866481ece120b
rramshinha001 <rram.shinha3@gmail.com>
2020-06-19 16:52:54 +0700

    job 6

---
+++ job6/src/pages/tabs/tabs.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { HomePage } from '../home/home';
+import { MyproductPage } from '../myproduct/myproduct';
+import { CovidinfoPage } from '../covidinfo/covidinfo';
+import { MyprofilePage } from '../myprofile/myprofile';
+
+/**
+ * Generated class for the TabsPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+  templateUrl: 'tabs.html'
+ })
+ export class TabsPage {
+  tab1Root = HomePage;
+  tab2Root = MyproductPage;
+  tab3Root = CovidinfoPage;
+  tab4Root = MyprofilePage;
+  constructor() {
+  }
+ }
